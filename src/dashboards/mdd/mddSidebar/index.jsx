c1beaeb5d6b7cdafa180f79f074a1d42aaf653f9
Rakshita Kanwar <rakshitakanwar09@gmail.com>
2025-05-25 15:05:50 +0530

    sidebar items mdd

--- src/dashboards/mdd/mddSidebar/index.jsx
+++ src/dashboards/mdd/mddSidebar/index.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { RiDashboardLine } from "react-icons/ri";
-import { TbTargetArrow } from "react-icons/tb";
-import { CgProfile } from "react-icons/cg";
 import { TfiLock } from "react-icons/tfi";
-import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { AppProvider, DashboardLayout } from "@toolpad/core";
 import PropTypes from "prop-types";
@@ -16,14 +12,23 @@ import SalesDashboard from "../../../pages/Dealer/SalesDashboard";
 import Profile from "../../../pages/Dealer/Profile";
 import SchemeLetters from "../../../pages/Dealer/SchemeLetters";
 import Targets from "../../../pages/Dealer/Targets";
-import { LuClipboardList } from "react-icons/lu";
-import { MdOutlineShoppingBag } from "react-icons/md";
 import MddHeader from "../mddHeader";
 import { createTheme } from "@mui/material/styles";
 import { Stack } from "@mui/material";
-import { SlCalculator } from "react-icons/sl";
 import Calculator from "../../../pages/Dealer/Calculator";
 import FinanceDashboard from "../../../pages/Mdd/financeDashboard";
+
+import { RiDashboardLine } from "react-icons/ri";
+import { MdOutlineShoppingBag, MdOutlinePayments } from "react-icons/md";
+import { LuClipboardList } from "react-icons/lu";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+import { TbTargetArrow } from "react-icons/tb";
+import { SlCalculator } from "react-icons/sl";
+import { CgProfile } from "react-icons/cg";
+import { BsFileEarmarkArrowDown, BsFileEarmarkArrowUp } from "react-icons/bs";
+import { PiSirenLight } from "react-icons/pi";
+import { GiReceiveMoney } from "react-icons/gi";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
 
 const iconStyle = { color: "rgba(249, 64, 8, 0.62)" };
 
@@ -60,7 +65,7 @@ const demoTheme = createTheme({
 
 const MddSidebar = [
   { kind: "header", title: "Distributor Panel" },
-  {
+ {
     segment: "dealer/dashboard",
     title: "Dashboard",
     icon: <RiDashboardLine size={22} {...iconStyle} />,
@@ -77,23 +82,38 @@ const MddSidebar = [
   },
   {
     segment: "dealer/scheme-letters",
-    title: "Scheme Letters",
-    icon: <LiaFileInvoiceDollarSolid size={22} {...iconStyle} />,
+    title: "Credit Notes",
+    icon: <BsFileEarmarkArrowDown size={22} {...iconStyle} />,
   },
   {
     segment: "dealer/targets",
-    title: "Targets",
+    title: "Purchase History",
     icon: <TbTargetArrow size={22} {...iconStyle} />,
   },
   {
     segment: "dealer/calculator",
-    title: "Calculator",
-    icon: <SlCalculator size={22} {...iconStyle} />,
+    title: "Pending Payments",
+    icon: <MdOutlinePayments size={22} {...iconStyle} />,
   },
   {
     segment: "dealer/profile",
-    title: "Profile",
-    icon: <CgProfile size={22} {...iconStyle} />,
+    title: "Debit Notes",
+    icon: <BsFileEarmarkArrowUp size={22} {...iconStyle} />,
+  },
+  {
+    segment: "dealer/profile",
+    title: "DOA Claims",
+    icon: <PiSirenLight size={22} {...iconStyle} />,
+  },
+  {
+    segment: "dealer/profile",
+    title: "CN Working",
+    icon: <GiReceiveMoney size={22} {...iconStyle} />,
+  },
+  {
+    segment: "dealer/profile",
+    title: "Marketing Invoices",
+    icon: <HiOutlineSpeakerphone size={22} {...iconStyle} />,
   },
   {
     segment: "mdd/logout",
